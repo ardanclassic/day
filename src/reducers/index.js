c88714ef28218e9636d60d2bde97b9e7d1bdab89
ardanclassic <41061962+ardanclassic@users.noreply.github.com>
2020-07-03 17:04:28 +0700

    Update index.js

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,4 +1,5 @@
 import { combineReducers } from 'redux';
 import counter from './todo';
+import data from './data';
 
-export default combineReducers({ counter })
+export default combineReducers({ counter, data })
